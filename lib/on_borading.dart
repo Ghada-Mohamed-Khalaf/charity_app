a5f5a03737b78b3ec1bc66f7d8ae535fad19503b
Ghada-Mohamed-Khalaf <gmo625112@gmail.com>
2024-12-29 11:09:49 +0200

    create onBoarding view

--- lib/on_borading.dart
+++ lib/on_borading.dart
@@ -1,12 +1,12 @@
-// import 'package:flutter/material.dart';
-//
-// class OnBorading extends StatelessWidget {
-//   const OnBorading({super.key});
-//
-//   @override
-//   Widget build(BuildContext context) {
-//     return const Placeholder();
-//   }
-// }
-//
-//
+import 'package:flutter/material.dart';
+
+class OnBorading extends StatelessWidget {
+  const OnBorading({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
+
+
